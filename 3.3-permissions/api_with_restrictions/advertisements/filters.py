c24b8f67719e51a787f247fb513d2cfbd4d70acd
BroadName <flintbistvud@mail.ru>
2024-06-18 18:54:01 +0600

    3.3 - is done

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,12 +1,14 @@
-from django_filters import rest_framework as filters
+from django_filters import rest_framework as filters, DateFromToRangeFilter, ChoiceFilter
 
-from advertisements.models import Advertisement
+from advertisements.models import Advertisement, AdvertisementStatusChoices
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
-
-    # TODO: задайте требуемые фильтры
+    created_at = DateFromToRangeFilter()
+    # Поиск по статусу /?status=OPEN or CLOSED
+    status = ChoiceFilter(choices=AdvertisementStatusChoices)
 
     class Meta:
         model = Advertisement
+        fields = ['created_at', 'status', 'creator']
--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AnonymousUser
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from advertisements.models import Advertisement
 
@@ -27,7 +28,7 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """Метод для создания"""
-
+        validated_data.pop('user')
         # Простановка значения поля создатель по-умолчанию.
         # Текущий пользователь является создателем объявления
         # изменить или переопределить его через API нельзя.
@@ -39,7 +40,10 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
-
-        # TODO: добавьте требуемую валидацию
-
+        ADV_STATUS = 'OPEN'
+        ADV_LIMIT = 10
+        if data.get('status', 'OPEN') == ADV_STATUS:
+            amount_of_post = len(Advertisement.objects.filter(status=ADV_STATUS, creator=self.context['request'].user))
+            if amount_of_post >= ADV_LIMIT:
+                raise ValidationError(f"You can't have more than {ADV_LIMIT} opened posts.")
         return data
